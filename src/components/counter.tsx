b467532672b69a41dc2fe690b461aa09077bbf8c
Burak Sakalli <buraksakalli06@gmail.com>
2023-05-21 21:44:38 +0300

    feature: Implement vibrate feature on button click

--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -12,6 +12,10 @@ interface CounterProps {
 }
 
 export default function Counter({ count, setCount, candidate }: CounterProps) {
+  const vibrateDevice = () => {
+    if (navigator.vibrate) navigator.vibrate([10]);
+  };
+
   return (
     <div className="select-none">
       <div className="flex items-center justify-center">
@@ -26,7 +30,10 @@ export default function Counter({ count, setCount, candidate }: CounterProps) {
       <div className="bg-theme-gray p-2.5 rounded-md flex flex-col gap-2.5 mt-5">
         <Button
           className="bg-theme-green hover:bg-theme-green/80 active:bg-theme-green/60"
-          onClick={() => setCount(count + 1)}
+          onClick={() => {
+            setCount(count + 1);
+            vibrateDevice();
+          }}
         >
           <PlusIcon className="stroke-white" />
         </Button>
@@ -46,7 +53,10 @@ export default function Counter({ count, setCount, candidate }: CounterProps) {
           className={cn("bg-theme-red", {
             "opacity-50": count === 0,
           })}
-          onClick={() => setCount(count - 1)}
+          onClick={() => {
+            setCount(count - 1);
+            vibrateDevice();
+          }}
           disabled={count === 0}
         >
           <MinusIcon className="stroke-white" />
